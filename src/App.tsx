fa456318cfd95635a4fb57c746788bb23979b801
obaidallah <obaidalqershi1@gmail.com>
2026-02-14 16:01:22 +0300

    remove: tanstack/react-virtual it's not needed

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,16 @@ import treeData from './data/treeWithTime.json'
 import { TreeCanvas } from './components/TreeCanvas'
 import type { TreeNodeData } from './types/tree'
 import { TimeSeriesModal } from './components/TimeSeriesModal'
-import { AnimatePresence, motion } from 'motion/react'
+
 export default function App(){
   const [selectedNode, setSelectedNode] = useState<TreeNodeData |null>(null);
   return(
     <>
     <TreeCanvas data={treeData} onSelectNode={setSelectedNode} />
-    <AnimatePresence>
+
     {selectedNode &&(
       <TimeSeriesModal node={selectedNode} onClose={()=> setSelectedNode(null)} />
     )}
-
-
-</AnimatePresence>
     </>
   )
 }
--- src/components/TreeCanvas.tsx
+++ src/components/TreeCanvas.tsx
@@ -13,8 +13,6 @@ interface Props {
 export function TreeCanvas({ data, onSelectNode }: Props) {
   const svgRef = useRef<SVGSVGElement | null>(null)
   const gRef = useRef<SVGGElement | null>(null)
-  const  transFormRef = useRef<d3.ZoomTransform>(d3.zoomIdentity);
-  const viewportHieght = window.innerHeight
 
 
   const [expandedIds, setExpandedIds] = useState<Set<string>>(
@@ -32,7 +30,6 @@ export function TreeCanvas({ data, onSelectNode }: Props) {
     const zoom = d3.zoom<SVGSVGElement, unknown>()
       .scaleExtent([0.2, 2])
       .on("zoom", (event) => {
-        transFormRef.current = event.transform;
         d3.select(gRef.current!)
           .attr("transform", event.transform)
       })
