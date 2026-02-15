8138628fefc9a45a66eda6306301d2221f470b41
obaidallah <obaidalqershi1@gmail.com>
2026-02-15 20:29:33 +0300

    add animation to the nodes

--- src/components/TreeNode.tsx
+++ src/components/TreeNode.tsx
@@ -1,48 +1,56 @@
-import type { PositionedNode } from "../types/tree";
+import { useEffect, useRef } from "react"
+import type { PositionedNode } from "../types/tree"
 
-interface Props{
-  node: PositionedNode,
-  isExpanded: boolean,
-  onToggle: (id: string) => void;
-  onSelect: (data: PositionedNode["data"]) => void;
+interface Props {
+  node: PositionedNode
+  isExpanded: boolean
+  onToggle: (id: string) => void
+  onSelect: (data: PositionedNode["data"]) => void
 }
 
 export function TreeNode({
   node,
   isExpanded,
   onToggle,
-  onSelect
-}: Props){
+  onSelect,
+}: Props) {
+  const gRef = useRef<SVGGElement | null>(null)
 
   const mainRadius = 24
   const toggleRadius = 10
   const gap = -4
-  return(
-    <g transform={`translate(${node.x}, ${node.y})`} >
-      <circle 
+
+  useEffect(() => {
+    if (!gRef.current) return
+
+    gRef.current.style.transition =
+      "transform 300ms cubic-bezier(0.4, 0, 0.2, 1)"
+
+    gRef.current.style.transform =
+      `translate(${node.x}px, ${node.y}px)`
+  }, [node.x, node.y])
+
+  return (
+    <g ref={gRef}>
+      <circle
         r={mainRadius}
-        fill='#e04f20'
+        fill="#e04f20"
         stroke="black"
         strokeWidth={1.5}
-        onClick={()=> {
-          onSelect(node.data)
-        }}
-        cursor='pointer'/>
-          
-          {!!node.data.children?.length && (
+        cursor="pointer"
+        onClick={() => onSelect(node.data)}
+      />
+
+      {!!node.data.children?.length && (
         <g
           transform={`translate(0, ${mainRadius + gap + toggleRadius})`}
           cursor="pointer"
-          onClick={() => onToggle(node.id)}
+          onClick={() => {
+            onToggle(node.id)
+          }}
         >
-          <circle
-            r={toggleRadius}
-            fill="#333"
-          />
-
+          <circle r={toggleRadius} fill="#333" />
           <text
-            x={0}
-            y={0}
             textAnchor="middle"
             dominantBaseline="middle"
             fill="white"
@@ -53,25 +61,15 @@ export function TreeNode({
           </text>
         </g>
       )}
-  <text
-    x={0}
-    y={0}
-    textAnchor="middle"
-    dominantBaseline="middle"
-    fill="white"
-    fontSize={8}
-    pointerEvents="none"
-  >
-    {node.name}
-  </text>
-        <title>
+      <text
+        textAnchor="middle"
+        dominantBaseline="middle"
+        fill="white"
+        fontSize={8}
+        pointerEvents="none"
+      >
         {node.name}
-        {"\n"}
-        ID: {node.id}
-        {"\n"}
-      </title>
-
-
+      </text>
     </g>
   )
 }
